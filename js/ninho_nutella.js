ed7b3d4921dfdb2e17ff5e64eb2a823b7285dd47
Gabriela Rankel <gabriela_rankel@estudante.sesisenai.org.br>
2026-04-01 10:35:53 -0300

    concertando

--- js/ninho_nutella.js
+++ js/ninho_nutella.js
@@ -1,6 +1,6 @@
-import Sorvete from "../modules/Sorvete.js"
-import Receita from "../modules/Receita.js"
-import Custo from "../modules/Custo.js"
+import Sorvete from "../models/Sorvete.js"
+import Receita from "../models/Receita.js"
+import Custo from "../models/Custo.js"
 
 const botaoCalcular = document.getElementById('botao_calcular')
 const botaoLimpar = document.getElementById('botao_limpar')
@@ -44,50 +44,108 @@ botaoCalcular.addEventListener('click', () => {
     const custoTotal = custo.formatar(custo.totalCusto)
     const custoUnitario = custo.formatar(custo.calcularCustoPorPote(qtdeSorvete))
 
-    const relatorioNaTela = `
-        <h3>Relatório: ${TONELADAS} Tonelada(s)</h3>
-        <p><strong>Rendimento:</strong> ${qtdeSorvete} potes</p>
-        <p><strong>Custo total:</strong> R$ ${custoTotal}</p>
-        <p><strong>Custo por pote:</strong> R$ ${custoUnitario}</p>
-        
-        <br>
-        <h4>Ingredientes</h4>
-        <table>
-            <thead>
-                <tr>
-                    <th>Ingrediente</th>
-                    <th class="td-label">Quantidade (kg)</th>
-                    <th>Custo (R$)</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td  class="td-label">Leite</td>
-                    <td>${(qtdeIngredientes.leite / 1000).toFixed(2)}</td>
-                    <td>${custo.formatar(precosIngredientes.leite)}</td>
-                </tr>
-                <tr>
-                    <td class="td-label">Creme</td>
-                    <td>${(qtdeIngredientes.creme / 1000).toFixed(2)}</td>
-                    <td>${custo.formatar(precosIngredientes.creme)}</td>
-                </tr>
-                <tr>
-                    <td class="td-label">Açúcar</td>
-                    <td>${(qtdeIngredientes.acucar / 1000).toFixed(2)}</td>
-                    <td>${custo.formatar(precosIngredientes.acucar)}</td>
-                </tr>
-                <tr>
-                    <td class="td-label">Leite em pó</td>
-                    <td>${(qtdeIngredientes.leiteEmPo / 1000).toFixed(2)}</td>
-                    <td>${custo.formatar(precosIngredientes.leiteEmPo)}</td>
-                </tr>
-                <tr>
-                    <td class="td-label">Nutella</td>
-                    <td>${(qtdeIngredientes.nutella / 1000).toFixed(2)}</td>
-                    <td>${custo.formatar(precosIngredientes.nutella)}</td>
-                </tr>
-            </tbody>
-        </table>
+   const relatorioNaTela = `
+<section class="tabela_resultado">
+
+  <h3 class="h3Tabela">Relatório: ${TONELADAS} Tonelada(s)</h3>
+
+  <table class="calc-table">
+    
+    <!-- ===== QUANTIDADE ===== -->
+    <thead>
+      <tr>
+        <th class="th-label">QUANTIDADE</th>
+        <th>POR POTE</th>
+        <th>POR TONELADA</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="td-label">LEITE:</td>
+        <td>${(qtdeIngredientes.leite / qtdeSorvete / 1000).toFixed(3)}</td>
+        <td>${(qtdeIngredientes.leite / 1000).toFixed(2)}</td>
+      </tr>
+      <tr>
+        <td class="td-label">CREME:</td>
+        <td>${(qtdeIngredientes.creme / qtdeSorvete / 1000).toFixed(3)}</td>
+        <td>${(qtdeIngredientes.creme / 1000).toFixed(2)}</td>
+      </tr>
+      <tr>
+        <td class="td-label">AÇÚCAR:</td>
+        <td>${(qtdeIngredientes.acucar / qtdeSorvete / 1000).toFixed(3)}</td>
+        <td>${(qtdeIngredientes.acucar / 1000).toFixed(2)}</td>
+      </tr>
+      <tr>
+        <td class="td-label">LEITE EM PÓ:</td>
+        <td>${(qtdeIngredientes.leiteEmPo / qtdeSorvete / 1000).toFixed(3)}</td>
+        <td>${(qtdeIngredientes.leiteEmPo / 1000).toFixed(2)}</td>
+      </tr>
+      <tr>
+        <td class="td-label">NUTELLA:</td>
+        <td>${(qtdeIngredientes.nutella / qtdeSorvete / 1000).toFixed(3)}</td>
+        <td>${(qtdeIngredientes.nutella / 1000).toFixed(2)}</td>
+      </tr>
+    </tbody>
+
+    <!-- ===== PREÇO ===== -->
+    <thead>
+      <tr>
+        <th class="th-label">PREÇO</th>
+        <th>POR POTE</th>
+        <th>POR TONELADA</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="td-label">LEITE:</td>
+        <td>${custo.formatar(precosIngredientes.leite / qtdeSorvete)}</td>
+        <td>${custo.formatar(precosIngredientes.leite)}</td>
+      </tr>
+      <tr>
+        <td class="td-label">CREME:</td>
+        <td>${custo.formatar(precosIngredientes.creme / qtdeSorvete)}</td>
+        <td>${custo.formatar(precosIngredientes.creme)}</td>
+      </tr>
+      <tr>
+        <td class="td-label">AÇÚCAR:</td>
+        <td>${custo.formatar(precosIngredientes.acucar / qtdeSorvete)}</td>
+        <td>${custo.formatar(precosIngredientes.acucar)}</td>
+      </tr>
+      <tr>
+        <td class="td-label">LEITE EM PÓ:</td>
+        <td>${custo.formatar(precosIngredientes.leiteEmPo / qtdeSorvete)}</td>
+        <td>${custo.formatar(precosIngredientes.leiteEmPo)}</td>
+      </tr>
+      <tr>
+        <td class="td-label">NUTELLA:</td>
+        <td>${custo.formatar(precosIngredientes.nutella / qtdeSorvete)}</td>
+        <td>${custo.formatar(precosIngredientes.nutella)}</td>
+      </tr>
+      <tr class="tr-total">
+        <td class="td-label td-rs">R$</td>
+        <td>${custoUnitario}</td>
+        <td>${custoTotal}</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <!-- ===== RESUMO ===== -->
+  <div class="resumo">
+    <div class="resumo_secao">
+      <div class="resumo-title">TAMANHO</div>
+      <div class="resumo-value">Raio ${raio} / Altura ${altura}</div>
+    </div>
+    <div class="resumo_secao">
+      <div class="resumo-title resumo-qty">Quantidade</div>
+      <div class="resumo-value">${qtdeSorvete}</div>
+    </div>
+    <div class="resumo_secao">
+      <div class="resumo-title resumo-price">R$</div>
+      <div class="resumo-value">${custoTotal}</div>
+    </div>
+  </div>
+
+</section>
     `
 
     tabela_resultado.innerHTML = relatorioNaTela
@@ -98,5 +156,5 @@ botaoLimpar.addEventListener('click', () => {
     document.getElementById('raio').value = 35
     document.getElementById('altura').value = 0.5
 
-    tabela_resultado.innerHTML = "<p>Insira os dados e clique em calcular.</p>"
+    tabela_resultado.innerHTML = ""
 })
